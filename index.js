06368ea9011ceffe32ebbea8fa7512028940aab1
ratroot92 <zcomisb.007@gmail.com>
2020-01-15 22:00:53 -0800

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class App extends Component {
   constructor() {
@@ -54,12 +59,12 @@ handleSubmit=(event)=>{
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-  <div class="form-group form-inline">
-<label>Name : </label>
-<input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} className="form-control"
+  <div className="form-group form-inline p-2">
+<label className="text-white btn btn-secondary font-weight-bold col-4 ">Name : </label>
+<input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} className="form-control col-8"
  />
- <div>
- {this.state.err_name ? (<span>{this.state.err_name}</span>):null}
+ <div className="p-0 m-0 mt-2 ">
+ {this.state.err_name ? (<span className="alert alert-warning font-weight-bold  m-0 p-1">{this.state.err_name}</span>):null}
  </div>
   </div>
 
@@ -67,7 +72,12 @@ handleSubmit=(event)=>{
 
 <input type="submit" className="btn btn-primary" value="submit" />
         </form>
+        
       </div>
+
+
+
+
     );
   }
 }
