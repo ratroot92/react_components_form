6ab235b19a391721ac4ee0d31845e162488f8fdd
ratroot92 <zcomisb.007@gmail.com>
2020-01-15 21:27:18 -0800

    2 files modified

--- index.js
+++ index.js
@@ -7,19 +7,65 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'asdasd'
+      name: '',
+      err_name:'',
+
+
+isvalid:true,
+
+
     };
   }
+valdiationcheck=()=>{
+  console.log("checking form validation ")
+  let isvalid=false;
+  if(this.state.name ===''){
+    this.setState({
+      err_name:'feild is required',
+    });
 
+    isvalid=false;
+  }
+
+
+
+  return isvalid;
+}
+
+handleSubmit=(event)=>{
+  event.preventDefault();
+
+  const isVerified=this.valdiationcheck();
+  if(isVerified){
+    console.log("isverfied_value =" +isVerified);
+  }
+  else{
+  console.log(this.state);
+  }
+
+}
+  handleChange=(event)=>{
+    this.setState({
+      [event.target.name]:event.target.name
+    });
+    console.log(event.target.name+" = "+event.target.value);
+  };
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
   <div class="form-group form-inline">
 <label>Name : </label>
-<input type="text" name="name" id="name" value={this.onChnage}
+<input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} className="form-control"
+ />
+ <div>
+ {this.state.err_name ? (<span>{this.state.err_name}</span>):null}
+ </div>
   </div>
 
+
+
+<input type="submit" className="btn btn-primary" value="submit" />
         </form>
       </div>
     );
